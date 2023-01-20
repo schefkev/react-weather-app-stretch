2b288b15fa20a50837aec802434168ed1d020d22
Kevin Toth <kevin.toth16@gmail.com>
2023-01-20 16:43:16 +0100

    Add weather objects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import { DateTime } from 'luxon';
+import { useEffect, useState } from 'react';
+import { Button, Card } from 'semantic-ui-react';
 
-function App() {
+export default function App() {
+  const weekDay = DateTime.now().weekdayLong;
+  const dateToday = DateTime.now().toFormat('MMMM dd, yyyy');
+  const timeNow = DateTime.now().toLocaleString(DateTime.TIME_24_WITH_SECONDS);
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
+  /* Get the current position */
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    /* declaring the async data fetching function */
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+      /* getting the data from the api */
+      const url = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=bf37b75730af7ca28743ab11a205c8ba`,
+      );
+      /* converting the data to json */
+      const json = await url.json();
+      /* setting state with the result */
+      setData(json);
+    };
+    fetchData().catch(console.error);
+  }, [lat, long]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {typeof data.main !== 'undefined' ? (
+        <Card.Content>
+          {/* HEADER ROW */}
+          <div className="top">
+            <header className="header">{data.name}</header>
+            <p className="time">{timeNow}</p>
+            <Button
+              className="button"
+              inverted
+              color="blue"
+              circular
+              icon="refresh"
+              onClick={refreshPage}
+            />
+          </div>
+          {/* First Row in the Card */}
+          <div className="flex-first">
+            <p className="day">
+              {weekDay}, <span className="day">{dateToday}</span>
+            </p>
+            <img
+              className="image"
+              alt=""
+              src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+            />
+          </div>
+          {/* Second Row in the Card */}
+          <div className="flex">
+            <p className="time">Temperature: {data.main.temp}&deg;C</p>
+            <p className="time">Clouds: {data.clouds.all}</p>
+          </div>
+          {/* Third Row in the Card */}
+          <div className="flex">
+            <p className="time">Humidity: {data.main.humidity}%</p>
+            <p className="time">
+              Wind Speed: {data.wind.speed}
+              <span className="time">Wind direction: {data.wind.deg}&deg;</span>
+            </p>
+          </div>
+        </Card.Content>
+      ) : (
+        <h1>Error</h1>
+      )}
     </div>
   );
 }
-
-export default App;
